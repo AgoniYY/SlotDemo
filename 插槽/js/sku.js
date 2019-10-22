c14446a2ae7a95f1e1c337c5c07f1e667fbd70e7
AgoniYY <45615076+AgoniYY@users.noreply.github.com>
2019-10-22 10:16:14 +0800

    Add files via upload

---
+++ 插槽/js/sku.js
@@ -0,0 +1,61 @@
+
+mainSKUList = [
+  {
+    id: 1,
+    name: '颜色'
+  },
+  {
+    id: 2,
+    name: '款式'
+  },
+  {
+    id: 3,
+    name: '尺寸'
+  }
+]
+childSKUList = {
+  1:[
+    {
+      childid: 11,
+      childsSpecName: '红',
+      checked: false,
+      disabled: false
+    },
+    {
+      childid: 12,
+      childsSpecName: '白',
+      checked: false,
+      disabled: false
+    }
+  ],
+  2: [
+    {
+      childid: 21,
+      childsSpecName: '乞丐款',
+      checked: false,
+      disabled: false
+    },
+    {
+      childid: 22,
+      childsSpecName: '爆炸款',
+      checked: false,
+      disabled: false
+    }
+  ],
+  3: [
+    {
+      childid: 31,
+      childsSpecName: 's',
+      checked: false,
+      disabled: false
+    },
+    {
+      childid: 32,
+      childsSpecName: 'm',
+      checked: false,
+      disabled: false
+    }
+  ]
+} 
+
+skuList = [['s', '红', '乞丐款'],['s', '红', '爆炸款'],['m', '红', '乞丐款'],['m', '红', '爆炸款'],['s', '白', '乞丐款'],['s', '白', '爆炸款']]
